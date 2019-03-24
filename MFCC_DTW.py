e04b7f3b30e4228e158b567115e473bcb8fe639f
kleinzcy <m15072762954@163.com>
2019-03-24 19:50:30 +0800

    calculate the distance between different sample

--- MFCC_DTW.py
+++ MFCC_DTW.py
@@ -6,22 +6,32 @@
 # @Software: PyCharm
 
 import os
+import time
 from utils.tools import read
-from utils.processing import enframe
+from utils.processing import enframe, stMFCC, mfccInitFilterBanks
 from sklearn.preprocessing import OrdinalEncoder
-
 import numpy as np
 from scipy.fftpack import fft
-from scipy.fftpack.realtransforms import dct
-
+from scipy.spatial.distance import euclidean
+from dtw import dtw
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 eps = 1e-8
+def get_time(start_time=None):
+    if start_time == None:
+        return time.time()
+    else:
+        return time.time() - start_time
+
+
 def load_wav(path='dataset/ASR'):
     """
     load data from dataset/ASR
     :param path: the path of dataset
     :return: x is train data, y_label is the label of x
     """
+    start_time = get_time()
     wav_dir = os.listdir(path)
     y_label = []
     x = []
@@ -29,93 +39,75 @@ def load_wav(path='dataset/ASR'):
     for _dir in wav_dir:
         for _path in os.listdir(os.path.join(path, _dir)):
             _, data = read(os.path.join(path, _dir, _path))
-            x.append(data[:, 0])
+            x.append(MFCC(data[:, 0]))
             y_label.append(_path.split('.')[0][:-1])
             del data
 
     y_label = np.array(y_label)
     y_label = enc.fit_transform(y_label.reshape(-1, 1))
+    print('loading data and extract mfcc feature spend {}s'.format(get_time(start_time)))
     return x,y_label
 
-def mfccInitFilterBanks(fs, nfft):
-    """
-    Computes the triangular filterbank for MFCC computation
-    (used in the stFeatureExtraction function before the stMFCC function call)
-    This function is taken from the scikits.talkbox library (MIT Licence):
-    https://pypi.python.org/pypi/scikits.talkbox
-    """
-    # filter bank params:
-    lowfreq = 133.33
-    linsc = 200/3.
-    logsc = 1.0711703
-    numLinFiltTotal = 13
-    numLogFilt = 27
 
-    if fs < 8000:
-        nlogfil = 5
-
-    # Total number of filters
-    nFiltTotal = numLinFiltTotal + numLogFilt
-
-    # Compute frequency points of the triangle:
-    freqs = np.zeros(nFiltTotal+2)
-    freqs[:numLinFiltTotal] = lowfreq + np.arange(numLinFiltTotal) * linsc
-    freqs[numLinFiltTotal:] = freqs[numLinFiltTotal-1] * logsc ** np.arange(1, numLogFilt + 3)
-    heights = 2./(freqs[2:] - freqs[0:-2])
-
-    # Compute filterbank coeff (in fft domain, in bins)
-    fbank = np.zeros((nFiltTotal, nfft))
-    nfreqs = np.arange(nfft) / (1. * nfft) * fs
-
-    for i in range(nFiltTotal):
-        lowTrFreq = freqs[i]
-        cenTrFreq = freqs[i+1]
-        highTrFreq = freqs[i+2]
-
-        lid = np.arange(np.floor(lowTrFreq * nfft / fs) + 1,
-                           np.floor(cenTrFreq * nfft / fs) + 1,
-                                       dtype=np.int)
-        lslope = heights[i] / (cenTrFreq - lowTrFreq)
-        rid = np.arange(np.floor(cenTrFreq * nfft / fs) + 1,
-                                       np.floor(highTrFreq * nfft / fs) + 1,
-                                       dtype=np.int)
-        rslope = heights[i] / (highTrFreq - cenTrFreq)
-        fbank[i][lid] = lslope * (nfreqs[lid] - lowTrFreq)
-        fbank[i][rid] = rslope * (highTrFreq - nfreqs[rid])
-
-    return fbank, freqs
-
-
-def stMFCC(X, fbank, n_mfcc_feats):
-    """
-    Computes the MFCCs of a frame, given the fft mag
-    ARGUMENTS:
-        X:        fft magnitude abs(FFT)
-        fbank:    filter bank (see mfccInitFilterBanks)
-    RETURN
-        ceps:     MFCCs (13 element vector)
-    Note:    MFCC calculation is, in general, taken from the
-             scikits.talkbox library (MIT Licence),
-    #    with a small number of modifications to make it more
-         compact and suitable for the pyAudioAnalysis Lib
-    """
-
-    mspec = np.log10(np.dot(X, fbank.T)+eps)
-    ceps = dct(mspec, type=2, norm='ortho', axis=-1)[:n_mfcc_feats]
-    return ceps
-
-def MFCC(signal, fs, win, step):
-    nFFT = int(win / 2)
+def MFCC(raw_signal, fs=16000, frameSize=400, step=160):
+    nFFT = int(frameSize / 2)
     [fbank, freqs] = mfccInitFilterBanks(fs, nFFT)
     n_mfcc_feats = 13
 
+    signal = enframe(raw_signal, frameSize, step)
     feature = []
-    for x in signal:
+    for frame in range(signal.shape[1]):
+        x = signal[:, frame]
         X = abs(fft(x))  # get fft magnitude
         X = X[0:nFFT]  # normalize fft
         X = X / len(X)
         feature.append(stMFCC(X, fbank, n_mfcc_feats))
 
+    feature = np.array(feature)
+    return feature.flatten()
+
+
+def distance_dtw(sample_x, sample_y, show=False):
+    """
+    calculate the distance between sample_x and sample_y using dtw
+    :param sample_x: ndarray, mfcc feature for each frame
+    :param sample_y: the same as sample_x
+    :param show: bool, if true, show the path
+    :return: the euclidean distance
+    """
+    # euclidean_norm = lambda x, y: np.abs(x - y)
+    d, cost_matrix, acc_cost_matrix, path = dtw(sample_x, sample_y, dist=euclidean)
+    if show:
+        plt.imshow(acc_cost_matrix.T, origin='lower', cmap='gray', interpolation='nearest')
+        plt.plot(path[0], path[1], 'w')
+        plt.show()
+    return d
+
+
+def distance_all(data):
+    """
+    calculate the distance of all data
+    :param data: input data, list, mfcc feature of all audio
+    :return: the distance matrix
+    """
+    start_time = get_time()
+    distance = np.zeros((len(data), len(data)))
+    for index, sample_x in enumerate(data):
+        col = index + 1
+        for sample_y in data[col:]:
+            distance[index, col] = distance_dtw(sample_x, sample_y)
+            distance[col, index] = distance[index, col]
+            col += 1
+    print('cost {}s'.format(get_time(start_time)))
+    return distance
+
+
 if __name__=='__main__':
     x,y = load_wav()
-    print(len(x), x[0].shape, y)
+    test = [x[0], x[1], x[5], x[6], x[10], x[11], x[15], x[16]]
+    distance = distance_all(test)
+    np.savetxt('dis.csv', X=distance, delimiter=',')
+
+
+    # print(distance_dtw(x[0], x[1], show=True))
+    # print(distance_dtw(x[0], x[5], show=True))
--- utils/processing.py
+++ utils/processing.py
@@ -8,10 +8,14 @@
 import numpy as np
 import math
 from scipy import signal
-from tools import read,play
+from scipy.io import wavfile
+# import read,play
+from scipy.fftpack.realtransforms import dct
+from scipy.fftpack import fft
+import matplotlib.pyplot as plt
+
+eps = 1e-8
 ## 语音的预处理函数
-
-
 def enframe(wavData, frameSize=400, step=160):
     """
     frame the wav data, according to frameSize and overlap
@@ -32,9 +36,103 @@ def enframe(wavData, frameSize=400, step=160):
     return frameData
 
 
+# frequency domain feature
+def mfccInitFilterBanks(fs, nfft):
+    """
+    Computes the triangular filterbank for MFCC computation
+    (used in the stFeatureExtraction function before the stMFCC function call)
+    This function is taken from the scikits.talkbox library (MIT Licence):
+    https://pypi.python.org/pypi/scikits.talkbox
+    """
+    # filter bank params:
+    lowfreq = 133.33
+    linsc = 200/3.
+    logsc = 1.0711703
+    numLinFiltTotal = 13
+    numLogFilt = 27
+
+    if fs < 8000:
+        nlogfil = 5
+
+    # Total number of filters
+    nFiltTotal = numLinFiltTotal + numLogFilt
+
+    # Compute frequency points of the triangle:
+    freqs = np.zeros(nFiltTotal+2)
+    freqs[:numLinFiltTotal] = lowfreq + np.arange(numLinFiltTotal) * linsc
+    freqs[numLinFiltTotal:] = freqs[numLinFiltTotal-1] * logsc ** np.arange(1, numLogFilt + 3)
+    heights = 2./(freqs[2:] - freqs[0:-2])
+
+    # Compute filterbank coeff (in fft domain, in bins)
+    fbank = np.zeros((nFiltTotal, nfft))
+    nfreqs = np.arange(nfft) / (1. * nfft) * fs
+
+    for i in range(nFiltTotal):
+        lowTrFreq = freqs[i]
+        cenTrFreq = freqs[i+1]
+        highTrFreq = freqs[i+2]
+
+        lid = np.arange(np.floor(lowTrFreq * nfft / fs) + 1,
+                           np.floor(cenTrFreq * nfft / fs) + 1,
+                                       dtype=np.int)
+        lslope = heights[i] / (cenTrFreq - lowTrFreq)
+        rid = np.arange(np.floor(cenTrFreq * nfft / fs) + 1,
+                                       np.floor(highTrFreq * nfft / fs) + 1,
+                                       dtype=np.int)
+        rslope = heights[i] / (highTrFreq - cenTrFreq)
+        fbank[i][lid] = lslope * (nfreqs[lid] - lowTrFreq)
+        fbank[i][rid] = rslope * (highTrFreq - nfreqs[rid])
+
+    return fbank, freqs
+
+
+def stMFCC(X, fbank, n_mfcc_feats):
+    """
+    Computes the MFCCs of a frame, given the fft mag
+    ARGUMENTS:
+        X:        fft magnitude abs(FFT)
+        fbank:    filter bank (see mfccInitFilterBanks)
+    RETURN
+        ceps:     MFCCs (13 element vector)
+    Note:    MFCC calculation is, in general, taken from the
+             scikits.talkbox library (MIT Licence),
+    #    with a small number of modifications to make it more
+         compact and suitable for the pyAudioAnalysis Lib
+    """
+
+    mspec = np.log10(np.dot(X, fbank.T)+eps)
+    ceps = dct(mspec, type=2, norm='ortho', axis=-1)[:n_mfcc_feats]
+    return ceps
+
+def test():
+    path = '../dataset/ASR/zcy/zcy1.wav'
+    frameSize = 400
+    nFFT = int(frameSize/2)
+    fs, audio = wavfile.read(path)
+    audio = audio[:, 0]
+
+    [fbank, freqs] = mfccInitFilterBanks(fs, nFFT)
+    n_mfcc_feats = 13
+    x = audio[4000:4000+frameSize]
+    X = abs(fft(x))  # get fft magnitude
+    X = X[0:nFFT]  # normalize fft
+    X = X / len(X)
+    feature = stMFCC(X, fbank, n_mfcc_feats)
+    plt.figure()
+    plt.subplot(121)
+    plt.plot(X)
+    plt.subplot(122)
+    plt.plot(feature)
+    plt.show()
+
+
 if __name__=='__main__':
+    test()
+
+    """
     path = '../dataset/ASR/zcy/zcy1.wav'
     play(path)
     samprate, data = read(path)
     framedata = enframe(data[:,0])
     print(framedata.shape)
+    """
